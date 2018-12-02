ec83f026772ab1e12366c3b7f606c40b15b551af
ktsn <ktsn55@gmail.com>
2018-12-02 14:50:17 +0800

    fix: prior prettier rule on vue sfc

--- eslintrc.js
+++ eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    'ktsn-typescript',
-    'plugin:vue/recommended'
-  ],
+  extends: ['ktsn-typescript', 'plugin:vue/recommended', 'prettier/vue'],
   parserOptions: {
     parser: 'typescript-eslint-parser'
   }
