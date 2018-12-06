a222c17b728d0d6e6d2f56f049ade3e78028a6e5
ktsn <ktsn55@gmail.com>
2018-12-06 22:45:41 +0800

    exclude vue and vue-router component from pascal case rule

--- eslintrc.js
+++ eslintrc.js
@@ -2,5 +2,25 @@ module.exports = {
   extends: ['ktsn-typescript', 'plugin:vue/recommended', 'prettier/vue'],
   parserOptions: {
     parser: 'typescript-eslint-parser'
+  },
+  rules: {
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        ignores: [
+          // vue core
+          'keep-alive',
+          'transition',
+          'transition-group',
+          'component',
+          'slot'
+
+          // vue-router
+          'router-link',
+          'router-view'
+        ]
+      }
+    ]
   }
 }
