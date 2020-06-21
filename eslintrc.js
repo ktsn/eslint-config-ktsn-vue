49ae635b6b77fc43c31a45fbc5b4886642a093db
ktsn <ktsn55@gmail.com>
2020-06-21 17:10:49 +0800

    bump deps

--- eslintrc.js
+++ eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-  extends: ['ktsn-typescript', 'plugin:vue/recommended', 'prettier/vue'],
+  extends: ["ktsn-typescript", "plugin:vue/recommended", "prettier/vue"],
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: "@typescript-eslint/parser",
   },
   rules: {
-    'vue/component-name-in-template-casing': [
-      'error',
-      'PascalCase',
+    "vue/component-name-in-template-casing": [
+      "error",
+      "PascalCase",
       {
         ignores: [
           // vue core
-          'keep-alive',
-          'transition',
-          'transition-group',
-          'component',
-          'slot',
+          "keep-alive",
+          "transition",
+          "transition-group",
+          "component",
+          "slot",
 
           // vue-router
-          'router-link',
-          'router-view'
-        ]
-      }
-    ]
-  }
-}
+          "router-link",
+          "router-view",
+        ],
+      },
+    ],
+  },
+};
